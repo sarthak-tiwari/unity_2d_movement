932e4506c63b5b2d7d20a706f801c478399b8c7e
sarthak-tiwari <43050725+sarthak-tiwari@users.noreply.github.com>
2019-05-08 19:15:34 -0700

    Added console background and fixed camera zoom a bit

--- Python_Movement_Compiler/Constants.py
+++ Python_Movement_Compiler/Constants.py
@@ -3,6 +3,8 @@ from enum import Enum
 class DataType(Enum):
     DIRECTION = 'DIRECTION'
     MAGNITUDE = 'MAGNITUDE'
+    LOCATION = 'LOCATION'
+    OBJECT = 'OBJECT'
 
 # declaring token_type valid values
 class TokenType(Enum):
